6f1dad7ae996cda79820d4eee59944a206dca485
kamlesh9070 <kamlesh9070@gmail.comm>
2021-01-01 17:02:18 +0530

    Hardcoded alarm offset to dynamic

--- packages/firebase_messaging/android/src/main/java/org/dadabhagwan/AKonnect/constants/WSConstant.java
+++ packages/firebase_messaging/android/src/main/java/org/dadabhagwan/AKonnect/constants/WSConstant.java
@@ -3,9 +3,6 @@ package org.dadabhagwan.AKonnect.constants;
 public interface WSConstant {
 
 
-  String pullUrl = "http://fastest.dbf.ooo/api/pull_notifications/";
-  String logUrl = "https://akstats.dbf.ooo/";
-
   String STATUS_NOTIFIED = "Notified";
 
   String PUSHTYPE_FCM = "FCM";
